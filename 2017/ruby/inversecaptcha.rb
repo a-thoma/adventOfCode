01822a535f26b56028f78b88ccc7f8ff19a93c93
fudgyone <nfaggler@gmail.com>
2017-12-04 15:26:17 -0800

    Still working

--- 2017/ruby/inversecaptcha.rb
+++ 2017/ruby/inversecaptcha.rb
@@ -2,4 +2,27 @@
 
 # inversecaptcha.rb
 
-puts 'Hello, world!'
+# This is my implementation for Advent of Code: Day 1
+# (See: https://adventofcode.com/2017/day/1)
+
+# function to find matches and return the sum of their numbers
+# TODO: Implement this
+def match(array)
+	# might be able to be recursive! a[i] and a[i+1] instead of i in array
+	for i in array
+		# do matches here
+	end
+end
+
+# the examples from the problem description
+# TODO: get input from users (?)
+
+arr1 = [1, 1, 2, 2]
+arr2 = [1, 1, 1, 1]
+arr3 = [1, 2, 3, 4]
+arr4 = [9, 1, 2, 1, 2, 1, 2, 9]
+
+match(arr1)
+match(arr2)
+match(arr3)
+match(arr4)
