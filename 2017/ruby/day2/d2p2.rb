154328541ddf3b4dd9b1df4c9ee9c0c06d499469
fudgyone <nfaggler@gmail.com>
2017-12-06 13:42:05 -0800

    fixed comments in part 1, started part 2

--- 2017/ruby/day2/d2p2.rb
+++ 2017/ruby/day2/d2p2.rb
@@ -1,29 +1,18 @@
 #! /usr/bin/env ruby
 
-# d2p1.rb
+# d2p2.rb
 
 # This is my implementation for Advent of Code: Day 2 (part 1)
 # (See: https://adventofcode.com/2017/day/2)
 
-# function to compute the difference of the smallest & largest elements of
-# each line of our input
+# function to compute the quotient of the two evenly-divisible numbers
+# in each line of our input, and return their sum
 def checkSum(array)
-    # variable to hold result
-    result = 0
-    # get max and min, calculate their difference, and add it to result
-    for i in 0...array.length
-        lo, hi = array[i][0], array[i][0]
-        for j in 0...array[i].length
-            if array[i][j] > hi
-                hi = array[i][j]
-            end
-            if array[i][j] < lo
-                lo = array[i][j]
-            end
-        end
-        result += (hi - lo)
-    end
-    return result
+	for i in 0...array.length
+		for j in (i + 1)...array.length
+			# do stuff
+		end
+	end
 end
 
 # function to get our input
@@ -47,7 +36,6 @@ def getInput(array)
     end
 end
 
-
 # initialize a 2d array to hold our data
 arr = Array.new() {Array.new()}
 
@@ -55,4 +43,5 @@ arr = Array.new() {Array.new()}
 getInput(arr)
 
 # perform our operation and print the result
-puts checkSum(arr)
+checkSum(arr)
+
